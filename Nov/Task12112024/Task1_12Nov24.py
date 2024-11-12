00c6534c23c2ed84eb711992b880a13a3aab62d8
GirishSBhosale <girish1031@gmail.com>
2024-11-11 21:58:34 -0500

    task 9th Nov 2024

---
+++ Nov/Task12112024/Task1_12Nov24.py
@@ -0,0 +1,9 @@
+# Allow 18 and above to club
+
+# ask user to input age
+age = int(input('Please enter a persons age.'))
+
+if age >= 18 :
+    print ("welcome to our club!")
+else:
+    print("We are sorry you are not allowed!")
---
+++ Nov/Task12112024/Task2_12Nov2024.py
@@ -0,0 +1,7 @@
+#Task 3
+#Problem to find the max between two ( 3,4) → 4
+
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+
+print(max(num1,num2))
